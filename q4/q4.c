8415ce38879f9c09234a5e9a0625292cb874baa2
Ritvik Bansal <bansalritvik2007@gmail.com>
2026-04-12 20:10:41 +0530

    add code for q4

---
+++ q4/q4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<dlfcn.h> //for dynamic libraries
+#include<string.h>
+int main()
+{
+    char s2[7];
+    int n,m;
+    while(scanf("%s %d %d",s2,&n,&m)==3) //till 3 inputs are recieved
+    {
+        char s1[20]="./lib";
+        char s3[5]=".so";
+        strcat(s1,s2); // used to make desired file name
+        strcat(s1,s3); 
+        // s1 ="./lib<op>.so"
+        void *library=dlopen(s1,RTLD_LAZY);
+        if(!library) continue; // if doesn't exist go for next input
+        int(*operation)(int,int)=dlsym(library,s2);// getting operation function from library
+        printf("%d\n",operation(n,m));
+        dlclose(library); // closing for 2 gb limit
+    }
+    return 0;
+}
